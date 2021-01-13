3d913dd50157a5d6804074ed3bc8f7800370262e
陈垚樨 <yoosonchan@163.com>
2021-01-13 15:29:50 +0800

    build

---
+++ docs/.vuepress/dist/assets/js/7.74be06f9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{371:function(n,t,e){"use strict";e.r(t);var l=e(42),s=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"y-life-container"},[this._v("\n    hello world.\n")])}),[],!1,null,null,null);t.default=s.exports}}]);
